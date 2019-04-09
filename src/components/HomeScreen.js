3c0a078e921e61794bf493bdda5a3343b1b11340
ab <haseeb.lums@gmail.com>
2019-04-09 19:39:01 +0500

    add new files

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import AppBar from "./AppBar";
+import GroupCard from "./GroupCard";
+import {StyleSheet, View,} from 'react-native';
+
+export default class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.main}>
+                <AppBar navigation={this.props.navigation}/>
+                <GroupCard navigation={this.props.navigation}/>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        backgroundColor: 'antiquewhite',
+    }
+});
