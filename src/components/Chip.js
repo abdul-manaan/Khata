4fe24ef4c5a07c129baeb069a2b5a195b0c649ec
ab <haseeb.lums@gmail.com>
2019-04-09 19:41:11 +0500

    Learned to git with dummy changes

--- src/components/Chip.js
+++
@@ -1,8 +0,0 @@
-// import * as React from 'react';
-// import { Chip } from 'react-native-paper';
-//
-// const MyComponent = () => (
-//     <Chip icon="info" onPress={() => console.log('Pressed')}>Example Chip</Chip>
-// );
-//
-// export default MyComponent;
