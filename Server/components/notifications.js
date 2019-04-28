846416d629f9507aea9d1c7b3301211e63820ff7
Abdul Rafae Noor <38834969+RafaeNoor@users.noreply.github.com>
2019-04-28 16:08:53 +0500

    fetch notifications against userID

--- Server/components/notifications.js
+++ Server/components/notifications.js
@@ -14,8 +14,14 @@ async function add_notification(info, userID){
     });
 }
 
+async function fetch_notification(userID){
+    let snapshot = await firebase_conn.db.ref('users/'+userID+'/notifications').once('value');
+    return snapshot.val();
+}
+
 module.exports = {
     add_notification: add_notification,
+    fetch_notification: fetch_notification,
 };
 
 
