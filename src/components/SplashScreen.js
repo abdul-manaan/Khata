b90d06ed2a6a6b99f433676453730982324daff3
abdul-manaan <manan.lums@gmail.com>
2019-05-16 15:42:30 +0500

    Finish App | Haseeb , Rafae , Muzammil , Manan and Salman

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        backgroundColor: 'antiquewhite',
+        backgroundColor: '#aa2200',
     },
     logoIcon: {
         width: 200,
